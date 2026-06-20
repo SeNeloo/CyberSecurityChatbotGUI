8a76d3c55aac151abfdeaadd61e8bacf9c98a3c3
Senelo Malesa <ST10481230@myemeris.edu.za>
2026-06-20 04:42:20 +0200

    created task assistant data model

---
+++ CyberSecurityChatbotGUI/TaskItem.cs
@@ -0,0 +1,15 @@
+﻿namespace CyberSecurityChatbotGUI
+{
+    public class TaskItem
+    {
+        public int TaskID {get; set;}
+
+        public string Title {get; set;}
+
+        public string Description {get; set;}
+
+        public DateTime ReminderDate {get; set;}
+
+        public string Status {get; set;}
+    }
+}
